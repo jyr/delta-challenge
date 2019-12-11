da299bb79d43e8d9b606d2a0d98ab0b2f91c674a
Jyr Gaxiola <jyr.gaxiola@gmail.com>
2019-12-10 23:02:18 -0600

    chore(api-news): infrastructure base with a mock test

--- src/apis/search/tests/api/news/test_endpoints.py
+++ src/apis/search/tests/api/news/test_endpoints.py
@@ -1,7 +1,25 @@
 from django.urls import reverse
+
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 class NewsEndpointTest(APITestCase):
 
-    pass
+    @classmethod
+    def setUp(self):
+        super().setUpClass()
+
+        self.url =reverse(
+            'api_news:retrieve_news_by_keywords'
+        )
+
+    def test_get_news(self):
+        data = {
+            "keywords": ["one", "two", "three"]
+        }
+
+        response = self.client.get(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('news' in response.data)
+        self.assertTrue(len(response.data['news']) == 3)
